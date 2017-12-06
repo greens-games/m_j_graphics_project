a109cccaf060a994c9c9240645d542f860f5525a
mgreen72194 <34315251+mgreen72194@users.noreply.github.com>
2017-12-06 11:48:42 -0600

    Add files via upload

---
+++ assets/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public Vector3 move_speed = Vector3.zero;
+	public int health = 100;
+	public Vector3 jump_speed = Vector3.zero;
+
+	//int test =  transform.position.y;
+
+	// Use this for initialization
+	void Start () {
+		move_speed.x = 0.1f;
+		jump_speed.y = 0.1f;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			if(gameObject.GetComponent<SpriteRenderer> ().flipX) {
+				gameObject.GetComponent<SpriteRenderer> ().flipX = false;
+			}
+			transform.position += move_speed;
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			flip();
+			transform.position -= move_speed;
+		}
+
+		if (health == 0) {
+			Destroy(gameObject);
+		}
+
+	
+	}
+
+	void FixedUpdate() {
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += jump_speed;
+		}
+	}
+
+	void TakeDamage() {
+			health -= 1;
+	}
+
+	void flip() {
+		gameObject.GetComponent<SpriteRenderer> ().flipX = true;
+	}
+}
